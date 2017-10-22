737f365964c51eae20486a74fc38ffca2acae822
NataPrivate <natalsha07@mail.ru>
2017-10-22 15:40:05 +0300

    metrics taking added

--- src/main/java/serialization/Main.java
+++ src/main/java/serialization/Main.java
@@ -11,35 +11,47 @@ public class Main {
 
         Book bookCopied = bookOriginal.clone();
         Serializable tester;
+        long time = 0;
 
         tester = new JacksonSerialization();
-        testSerialization(bookOriginal, bookCopied, tester);
+        testSerialization(bookOriginal, bookCopied, tester, time);
 
         tester = new GSONSerialization();
-        testSerialization(bookOriginal, bookCopied, tester);
+        testSerialization(bookOriginal, bookCopied, tester, time);
 
         tester = new JSONSerialization();
-        testSerialization(bookOriginal, bookCopied, tester);
+        testSerialization(bookOriginal, bookCopied, tester, time);
     }
 
-    private static void testSerialization(Book bookOriginal, Book bookCopied, Serializable tester) {
-        toFile(tester, bookCopied);
-        bookCopied = fromFile(tester);
-        if (!bookOriginal.equals(bookCopied))
-            System.out.println("ERROR");
+    private static void testSerialization(Book bookOriginal, Book bookCopied, Serializable tester, long time) {
+        System.out.println(tester.getClass().getName());
+        for (int i = 0; i < 3; i++) {
+            time = System.nanoTime();
+            toFile(tester, bookCopied);
+            bookCopied = fromFile(tester);
+            if (!bookOriginal.equals(bookCopied))
+                System.out.println("ERROR");
+            System.out.println((System.nanoTime() - time) + " nanosec");
+            memoryConsumption();
+        }
     }
 
     private static void toFile(Serializable tester, Book book) {
-        try {
-            tester.bookSerialize(book);
-        }
-        catch (IOException e) { e.printStackTrace(); }}
+        try { tester.bookSerialize(book); }
+        catch (IOException e) { e.printStackTrace(); }
+    }
 
     private static Book fromFile(Serializable tester) {
         Book book = new Book();
-        try {
-            book = tester.bookDeserialize();
-        }
+        try { book = tester.bookDeserialize(); }
         catch (IOException | ParseException e) { e.printStackTrace(); }
-        return book;}
+        return book;
+    }
+
+    private static void memoryConsumption() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.printf("Memory used: %d B \n", memory);
+    }
 }
