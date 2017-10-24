e9851f3471d93e69cba953fa4111d325ddbbd142
NataPrivate <natalsha07@mail.ru>
2017-10-24 21:16:34 +0300

    + Map field coresponding changes in constructor and equals

--- src/main/java/serialization/Book.java
+++ src/main/java/serialization/Book.java
@@ -1,6 +1,8 @@
 package serialization;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 
 public class Book {
@@ -8,6 +10,7 @@ public class Book {
     private String name;
     private String author;
     private List<Chapter> chapters;
+    private Map<String, Object> map;
 
     public short getPageCount() {
         return pageCount;
@@ -21,18 +24,30 @@ public class Book {
     public List<Chapter> getChapters() {
         return chapters;
     }
+    public Map<String, Object> getMap() {
+        return map;
+    }
 
     public Book() {this((short) 0, "Unknown", "Unknown");}
     public Book(short pageCount, String name, String author) {
+        this(pageCount, name, author, null);
+    }
+    public Book(short pageCount, String name, String author, List<Chapter> chapters) {
         if (pageCount > 0)
             this.pageCount = pageCount;
         this.name = name;
         this.author =  author;
-        chapters = new ArrayList<>();
+        if (chapters == null)
+            this.chapters = new ArrayList<>();
+        else
+            this.chapters = chapters;
+        initMap();
     }
-    public Book(short pageCount, String name, String author, List<Chapter> chapters) {
-        this(pageCount, name, author);
-        this.chapters = chapters;
+    private void initMap() {
+        map = new HashMap();
+        map.put("pageCount", pageCount);
+        map.put("name", name);
+        map.put("author", author);
     }
 
     public void addChapter(Chapter chapter) {
@@ -44,19 +59,20 @@ public class Book {
 
     @Override
     public String toString() {
-        return "\'" + name + "\' written by " + author + ", " + chapters.size() + " chapter(s)";
+        return "\'" + name + "\' written by " + author + ", " + chapters.size() + " chapter(s), " + pageCount + "p.";
     }
 
+    @Override
     public Book clone() {
         ArrayList<Chapter> chapters = new ArrayList<>();
         for (Chapter chapter:this.chapters)
             chapters.add(chapter.clone());
-        return new Book(pageCount, name, author, chapters);
+        return new Book(this.pageCount, this.name, this.author, chapters);
     }
 
     public boolean equals(Book book) {
         if (this.pageCount == book.pageCount && this.name.equals(book.name) && this.author.equals(book.author)
-            && this.chapters.size() == book.chapters.size()) {
+            && this.chapters.size() == book.chapters.size() && this.map.equals(book.getMap())) {
             for (int i = 0; i < this.chapters.size(); i++)
                 if (!chapters.get(i).equals(book.chapters.get(i)))
                     return false;
