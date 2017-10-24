7bde5dd1b65444bd97cd7d70b2cd19fdee1b4684
NataPrivate <natalsha07@mail.ru>
2017-10-24 23:44:27 +0300

    + adopted for Map (internal class fixes source bug: return in.nextDouble();) + prettier output

--- src/main/java/serialization/GSONSerialization.java
+++ src/main/java/serialization/GSONSerialization.java
@@ -1,15 +1,17 @@
 package serialization;
 
-import com.google.gson.Gson;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import com.google.gson.*;
+import com.google.gson.internal.LinkedTreeMap;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.Type;
 
 public class GSONSerialization implements Serializable {
     @Override
     public void bookSerialize(Book book) throws IOException {
         try (FileWriter writer = new FileWriter("bookGSON.json")) {
-            Gson gson = new Gson();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
             gson.toJson(book, writer);
         }
     }
@@ -17,8 +19,49 @@ public class GSONSerialization implements Serializable {
     @Override
     public Book bookDeserialize() throws IOException {
         try (FileReader reader = new FileReader("bookGSON.json")) {
-            Gson gson = new Gson();
+            Gson gson = new GsonBuilder().enableComplexMapKeySerialization().registerTypeAdapter(new TypeToken<Map <String, Object>>(){}.getType(),  new MapDeserializerDoubleAsIntFix()).setPrettyPrinting().create();
             return gson.fromJson(reader, Book.class);
+        }
+    }
+    static class MapDeserializerDoubleAsIntFix implements JsonDeserializer<Map<String, Object>> {
+        @Override  @SuppressWarnings("unchecked")
+        public Map<String, Object> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return (Map<String, Object>) read(json);
+        }
+
+        Object read(JsonElement element) {
+            if (element.isJsonArray()) {
+                List<Object> list = new ArrayList<>();
+                JsonArray arr = element.getAsJsonArray();
+                for (JsonElement arrElement : arr)
+                    list.add(read(arrElement));
+                return list;
+            }
+            else if (element.isJsonObject()){
+                Map<String, Object> map = new LinkedTreeMap<>();
+                JsonObject obj = element.getAsJsonObject();
+                Set<Map.Entry<String, JsonElement>> entitySet = obj.entrySet();
+                for(Map.Entry<String, JsonElement> entry: entitySet)
+                    map.put(entry.getKey(), read(entry.getValue()));
+                return map;
+            }
+            else if (element.isJsonPrimitive()) {
+                JsonPrimitive prim = element.getAsJsonPrimitive();
+                if (prim.isBoolean())
+                    return prim.getAsBoolean();
+
+                else if (prim.isString())
+                    return prim.getAsString();
+
+                else if (prim.isNumber()) {
+                    Number num = prim.getAsNumber();
+                    if(Math.ceil(num.doubleValue())  == num.shortValue())
+                        return num.shortValue();
+                    else
+                        return num.doubleValue();
+                }
+            }
+            return null;
         }
     }
 }
