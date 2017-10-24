c8467c63519e3e2786a190de2310322388b4f27b
NataPrivate <natalsha07@mail.ru>
2017-10-24 21:19:32 +0300

    + adopted for Map + prettier output

--- src/main/java/serialization/JacksonSerialization.java
+++ src/main/java/serialization/JacksonSerialization.java
@@ -8,12 +8,14 @@ public class JacksonSerialization implements Serializable {
     @Override
     public void bookSerialize(Book book) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(new File("bookJackson.json"), book);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File("bookJackson.json"), book);
     }
 
     @Override
     public Book bookDeserialize() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         return mapper.readValue(new File("bookJackson.json"), Book.class);
     }
 }
